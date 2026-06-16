084c44745953473df42584d02be25558f9626143
Nischit007 <nischitpantha@gmail.com>
2026-06-16 21:14:33 +0545

    intial commit with fail : running test.sh script

---
+++ app.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+VERSION="1.0.0"
+
+echo "Application Version : $VERSION"
+echo "Hostname            : $(hostname)"
+echo "Date                : $(date)"
---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./app.sh > /dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+    echo "PASS"
+else
+    echo "FAIL"
+fi
